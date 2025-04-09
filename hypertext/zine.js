f0df7c5a784462bafaa37016a4e8ef1bf27081f2
zelda <126986145+zeldaskywalker@users.noreply.github.com>
2025-04-09 18:13:57 -0400

    Update zine.js
    only show website if width is minimum 1400px

--- hypertext/zine.js
+++ hypertext/zine.js
@@ -2,13 +2,13 @@ function handleMobileOrResize() {
   console.log('resizing...');
   const zineContainer = document.querySelector('#overall-zine-container');
   const mobileContainer = document.querySelector('#mobile');
-  if (window.innerWidth < 690) {
+  if (window.innerWidth < 1400) {
     zineContainer.style.display = 'none';
     mobileContainer.style.display = 'flex';
     mobileContainer.style['flex-direction'] = 'column';
     mobileContainer.style.padding = '15px';
     mobileContainer.style.margin = 'auto';
-  } else if (window.innerWidth >= 690) {
+  } else if (window.innerWidth >= 1400) {
     zineContainer.style.display = 'flex';
     mobileContainer.style.display = 'none';
   }
