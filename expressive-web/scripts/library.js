3670284dd0d4c79ff625c084750d6929b05d9a9a
zelda <126986145+zeldaskywalker@users.noreply.github.com>
2025-04-30 15:03:21 -0400

    some updates...

--- expressive-web/scripts/library.js
+++ expressive-web/scripts/library.js
@@ -16,7 +16,19 @@ async function getAllBooks() {
         var library_container = document.getElementById('library');
         var book_div = document.createElement('div');
         book_div.innerText = book.title;
+        book_div.className = "book_on_shelf";
         library_container.appendChild(book_div);
+
+        var open_book_container = document.getElementById('open-book');
+        var open_book_div = document.createElement('div');
+        open_book_div.className = "book_details";
+        open_book_div.innerText = book.initials + " read " + book.title + " by " + book.author + "." 
+        open_book_container.appendChild(open_book_div);
+
+        book_div.addEventListener("click", function() {
+          console.log('i am here');
+          open_book_div.style.display = "flex";
+        })
     })
   });
 }
@@ -24,3 +36,10 @@ async function getAllBooks() {
 getAllBooks();
 
 // TODO: on pressing a specific book, create a pop-up with all book info on right hand side
+// function click(book){
+//   var open_book_container = document.getElementById('open-book');
+//   var book_div = document.createElement('div');
+//   book_div.className = "book_details";
+//   book_div.innerText = book.initials + " read " + book.title + " by " + book.author + "." 
+//   open_book_container.appendChild(book_div);
+// }
